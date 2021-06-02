6730964380e77a1e6cf24bd9c939aa011670bb1e
Hoa <hoa@hoa-ThinkPad-T14-Gen-1>
2021-06-03 00:50:30 +0200

    multi-thread scraping

--- main.py
+++ main.py
@@ -1,36 +1,46 @@
 import get_book
-import urllib
 import get_category
 import utils
+from pymongo import MongoClient
+import time
+import concurrent.futures
 
+client = MongoClient()
+mydb = client["mydatabase"]
+mycol = mydb["books"]
 
 url = 'http://books.toscrape.com/'
 
 infos = [
-"product_page_url",
-"universal_product_code (upc)",
-"title",
-"price_including_tax",
-"price_excluding_tax",
-"number_available",
-"product_description",
-"category",
-"review_rating",
-"image_url",
+    "product_page_url",
+    "universal_product_code (upc)",
+    "title",
+    "price_including_tax",
+    "price_excluding_tax",
+    "number_available",
+    "product_description",
+    "category",
+    "review_rating",
+    "image_url",
 ]
 
-try:
-    list_of_books = get_category.get_all_books_url(utils.get_page_html('http://books.toscrape.com/catalogue/category/books/classics_6/index.html'))
-    books_dets = []
+def title_push(url):
+    title = get_book.scrape(url)['title']    
+    mydict = { "url": url, "title": title }
+    x = mycol.insert_one(mydict)
+
+list_of_books = get_category.get_all_books_url(utils.get_page_html('http://books.toscrape.com/catalogue/category/books/classics_6/index.html'))
+
+startTime = time.time()
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = []
     for url in list_of_books:
-        print(get_book.scrape(url)['title'])
-except Exception as e:
-    print(e)
-else:
-    print(books_dets)
+        futures.append(executor.submit(title_push, url=url))
 
-# try:
-#     urllib.request.urlretrieve('http://books.toscrape.com/media/cache/fe/72/fe72f0532301ec28892ae79a629a293c.jpg', './pic.jpg')
-# except Exception as e:
-#     print(e)
+executionTime = (time.time() - startTime)
 
+print('Execution time in seconds: ' + str(executionTime))
+print(f'len of books: {len(list_of_books)}')
+
+for x in mycol.find():
+    print(x)
