192631f611a57b85a5d60d6ec1ed2a331f8e880f
Yvon M <yvon.momboisse@knap.fr>
2021-06-02 07:37:37 +0200

    migrating to requests

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import requests
 
 def get_page_html(url: str) -> object:
     html = urlopen(url)
     return BeautifulSoup(html.read(), 'lxml')
+
+
+page = requests.get('http://books.toscrape.com/')
+print(page.text)
