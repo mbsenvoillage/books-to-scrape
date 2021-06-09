41ef429c8a56b50f8b29d63b15b4209cbc3b58b9
Yvon M <yvon.momboisse@knap.fr>
2021-06-09 08:26:52 +0200

    filewriter

--- file_writer.py
+++ file_writer.py
@@ -1,20 +1,12 @@
 import csv
+import logging
 
-with open('test.csv', encoding='utf-8-sig', mode='w') as csv_file:
-    fieldnames = ['emp_name', 'dept', 'birth_month']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-    writer.writeheader()
-    writer.writerow({'emp_name': 'John Smith', 'dept': 'Accounting', 'birth_month': 'November'})
-
-
-def touch_csv(filename):
-    with open(f"{filename}.csv", "w") as file:
-        pass
-
-    
-touch_csv('another')
-
-# def write_to_csv(fieldnames, obj):
-#     with open('')
-#     writer = csv.DictWriter()
+def write_file(filename, fieldnames, mode, array):
+    try:
+        with open(f"{filename}.csv", encoding='utf-8-sig', mode=mode) as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(array)
+    except Exception as e:
+        logging.error(e)
+        raise
